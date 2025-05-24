b30a7c6ffbc16c6794a39aac75056879062fe074
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-05-24 23:22:08 +0600

    configured tsup

---
+++ tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+	entry: ['src/index.ts'],
+	format: ['esm', 'cjs'],
+	dts: true,
+	clean: true,
+	sourcemap: true,
+	target: ['esnext'],
+	// splitting: false,
+	skipNodeModulesBundle: true,
+	// bundle: false,
+	shims: false,
+	outDir: 'dist',
+});
