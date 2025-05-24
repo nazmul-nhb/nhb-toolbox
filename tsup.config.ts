c09cb546b712895d7897eea0c978567115faa941
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-05-25 03:45:18 +0600

    updated tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,15 +3,15 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
 	entry: ['src/index.ts'],
 	format: ['esm', 'cjs'],
+	target: ['esnext'],
+	outDir: 'dist',
 	dts: true,
 	clean: true,
 	sourcemap: true,
-	target: ['esnext'],
 	// splitting: false,
-	skipNodeModulesBundle: true,
 	// bundle: false,
-	shims: true,
-	outDir: 'dist',
+	// shims: true,
+	skipNodeModulesBundle: true,
 	esbuildOptions(options) {
 		options.resolveExtensions = ['.ts', '.js', '.mts', '.mjs'];
 	},
