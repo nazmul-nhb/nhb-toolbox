4a4a7f019af08b3011531d9c883b9bcc4095ccbe
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-07-05 16:09:24 +0600

    chore(script): updated nhb-scripts to fix commit type selection issue

--- nhb.format.config.mjs
+++ nhb.format.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
 
 import { defineFormatConfig } from 'nhb-scripts';
-    
+
 export default defineFormatConfig({
     args: ['--write'],
-    files: ['src'],
+    files: ['src', 'scripts'],
     ignorePath: '.prettierignore',
 });
