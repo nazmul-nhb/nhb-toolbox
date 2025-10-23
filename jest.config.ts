a65748f686bb544858706f8ba37f8ce3d280f521
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-10-23 22:09:09 +0600

    `test(config):` reconfigured jest config file

--- jest.config.ts
+++ jest.config.ts
@@ -1,29 +1,22 @@
-/** @type {import('jest').Config} */
-export default {
+import type { Config } from 'jest';
+
+const config: Config = {
 	preset: 'ts-jest', // Handles TypeScript support
 	testEnvironment: 'node', // Use 'jsdom' if testing browser-related code
 
 	// File extensions Jest should recognize
-	moduleFileExtensions: [
-		'ts',
-		'tsx',
-		'js',
-		'cjs',
-		'mjs',
-		'jsx',
-		'json',
-		'node',
-	],
+	moduleFileExtensions: ['ts', 'tsx', 'js', 'cjs', 'mjs', 'jsx', 'json', 'node'],
 
 	// Test file patterns
 	testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
 
 	// Coverage settings
 	coverageDirectory: 'coverage',
-	collectCoverage: true,
+	// collectCoverage: true,
 	collectCoverageFrom: [
 		'src/**/*.{ts,tsx}', // Collect coverage from source files
 		'!src/index.ts', // Exclude main entry file
+		'!src/constants.ts', // Exclude exported constants file
 	],
 
 	// Module resolution settings
@@ -32,3 +25,5 @@ export default {
 	// Uncomment if you have setup files
 	// setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
 };
+
+export default config;
