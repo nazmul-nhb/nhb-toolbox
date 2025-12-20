f35fea63266a4b88c3310aa22dfea9c1aae63a13
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-12-21 00:42:32 +0600

    `feat(Chronos/plugins):` implemented 6 new methods in `bengaliPlugin`

--- src/date/plugins/bengaliPlugin.ts
+++ src/date/plugins/bengaliPlugin.ts
@@ -1,6 +1,18 @@
 // import { INTERNALS } from '../Chronos';
 import { _formatDateCore } from '../helpers';
-import type { $Chronos, $BnEn, BanglaDate, BanglaSeasonName, StrictFormat } from '../types';
+import type {
+	$Chronos,
+	$BnEn,
+	BanglaDate,
+	BanglaDayName,
+	BanglaMonth,
+	BanglaMonthDate,
+	BanglaMonthName,
+	BanglaSeasonName,
+	BanglaYear,
+	StrictFormat,
+} from '../types';
+import type { Chronos } from '../Chronos';
 import { BN_DAYS, BN_DIGITS, BN_MONTHS, BN_SEASONS } from '../constants';
 
 const YEAR_OFFSET = 593;
@@ -55,15 +67,25 @@ declare module '../Chronos' {
 
 		formatBangla(format: StrictFormat): string;
 
-		getBanglaYear(): number;
-		getBanglaMonth(): number;
-		getBanglaDay(): number;
+		getBanglaYear<Locale extends $BnEn = 'bn'>(
+			locale?: Locale
+		): Locale extends 'en' ? number : BanglaYear;
 
-		getBanglaMonthName(): string;
-		getBanglaMonthLength(): number;
-		getBanglaSeason(): string;
+		getBanglaMonth<Locale extends $BnEn = 'bn'>(
+			locale?: Locale
+		): Locale extends 'en' ? number : BanglaMonth;
 
-		isBanglaLeapYear(): boolean;
+		getBanglaDay<Locale extends $BnEn = 'bn'>(
+			locale?: Locale
+		): Locale extends 'en' ? number : BanglaMonthDate;
+
+		getBanglaDayName<Locale extends $BnEn = 'bn'>(locale?: Locale): BanglaDayName<Locale>;
+		getBanglaMonthName<Locale extends $BnEn = 'bn'>(
+			locale?: Locale
+		): BanglaMonthName<Locale>;
+		getBanglaSeasonName<Locale extends $BnEn = 'bn'>(
+			locale?: Locale
+		): BanglaSeasonName<Locale>;
 
 		startOfBanglaMonth(): Chronos;
 		endOfBanglaMonth(): Chronos;
@@ -77,18 +99,63 @@ declare module '../Chronos' {
 export const bengaliPlugin = ($Chronos: $Chronos): void => {
 	// const { internalDate } = $Chronos[INTERNALS];
 
-	$Chronos.prototype.toBangla = function <Locale extends $BnEn>(locale?: Locale) {
-		const gy = this.year;
-		const gm = this.isoMonth;
-		const gd = this.date;
+	const getBaseYear = (chronos: Chronos) => {
+		const gy = chronos.year;
+		const gm = chronos.isoMonth;
 
-		const utcTs = Date.UTC(gy, gm - 1, gd);
+		return gm < 4 || (gm === 4 && chronos.date < 14) ? gy - 1 : gy;
+	};
 
-		const bnYearBase = gm < 4 || (gm === 4 && gd < 14) ? gy - 1 : gy;
+	const selectMonthTable = (chronos: Chronos) => {
+		return chronos.isLeapYear() ? BN_POST_2019_LEAP : BN_POST_2019_NORMAL;
+	};
 
-		const monthTable = this.isLeapYear() ? BN_POST_2019_LEAP : BN_POST_2019_NORMAL;
+	const getUtcTs = (chronos: Chronos) => {
+		return Date.UTC(chronos.year, chronos.month, chronos.date);
+	};
 
-		let days = Math.floor((utcTs - Date.UTC(bnYearBase, 3, 14)) / MS_PER_DAY);
+	const getElapsedDays = (chronos: Chronos) => {
+		return Math.floor(
+			(getUtcTs(chronos) - Date.UTC(getBaseYear(chronos), 3, 14)) / MS_PER_DAY
+		);
+	};
+
+	const getBnMonthIdx = (chronos: Chronos) => {
+		const monthTable = selectMonthTable(chronos);
+
+		let days = getElapsedDays(chronos);
+		let month = 0;
+
+		while (days >= monthTable[month]) {
+			days -= monthTable[month];
+			month++;
+		}
+
+		return month;
+	};
+
+	$Chronos.prototype.getBanglaYear = function <Locale extends $BnEn>(locale?: Locale) {
+		const bnYearBase = getBaseYear(this);
+
+		const bnYear = bnYearBase - YEAR_OFFSET;
+
+		return (locale === 'en' ? bnYear : digitToBangla(bnYear)) as Locale extends 'en' ?
+			number
+		:	BanglaYear;
+	};
+
+	$Chronos.prototype.getBanglaMonth = function <Locale extends $BnEn>(locale?: Locale) {
+		const isoMonth = getBnMonthIdx(this) + 1;
+
+		return (locale === 'en' ? isoMonth : digitToBangla(isoMonth)) as Locale extends 'en' ?
+			number
+		:	BanglaMonth;
+	};
+
+	$Chronos.prototype.getBanglaDay = function <Locale extends $BnEn>(locale?: Locale) {
+		const monthTable = selectMonthTable(this);
+
+		let days = getElapsedDays(this);
 		let month = 0;
 
 		while (days >= monthTable[month]) {
@@ -97,35 +164,86 @@ export const bengaliPlugin = ($Chronos: $Chronos): void => {
 		}
 
 		const date = days + 1;
-		const isoMonth = month + 1;
-		const bnYear = bnYearBase - YEAR_OFFSET;
 
+		return (locale === 'en' ? date : digitToBangla(date)) as Locale extends 'en' ? number
+		:	BanglaMonthDate;
+	};
+
+	$Chronos.prototype.getBanglaDayName = function <Locale extends $BnEn>(locale?: Locale) {
 		const DAY = BN_DAYS[this.weekDay];
-		const MONTH = BN_MONTHS[month];
 
-		switch (locale) {
-			case 'en':
-				return {
-					year: bnYear,
-					month: isoMonth,
-					date: date,
-					monthName: MONTH.en,
-					dayName: DAY.en,
-					seasonName: getSeason(month, 'en'),
-					isLeapYear: this.isLeapYear(),
-				} as BanglaDate<Locale>;
+		return (locale === 'en' ? DAY.en : DAY.bn) as BanglaDayName<Locale>;
+	};
 
-			default:
-				return {
-					year: digitToBangla(bnYear),
-					month: digitToBangla(isoMonth),
-					date: digitToBangla(date),
-					monthName: MONTH.bn,
-					dayName: DAY.bn,
-					seasonName: getSeason(month),
-					isLeapYear: this.isLeapYear(),
-				} as BanglaDate<Locale>;
-		}
+	$Chronos.prototype.getBanglaMonthName = function <Locale extends $BnEn>(locale?: Locale) {
+		const MONTH = BN_MONTHS[getBnMonthIdx(this)];
+
+		return (locale === 'en' ? MONTH.en : MONTH.bn) as BanglaMonthName<Locale>;
+	};
+
+	$Chronos.prototype.getBanglaSeasonName = function <Locale extends $BnEn>(locale?: Locale) {
+		return getSeason(getBnMonthIdx(this), locale);
+	};
+
+	$Chronos.prototype.toBangla = function <Locale extends $BnEn>(locale?: Locale) {
+		// const gy = this.year;
+		// const gm = this.isoMonth;
+		// const gd = this.date;
+
+		// const utcTs = Date.UTC(gy, gm - 1, gd);
+
+		// const bnYearBase = gm < 4 || (gm === 4 && gd < 14) ? gy - 1 : gy;
+
+		// const monthTable = this.isLeapYear() ? BN_POST_2019_LEAP : BN_POST_2019_NORMAL;
+
+		// let days = Math.floor((utcTs - Date.UTC(bnYearBase, 3, 14)) / MS_PER_DAY);
+		// let month = 0;
+
+		// while (days >= monthTable[month]) {
+		// 	days -= monthTable[month];
+		// 	month++;
+		// }
+
+		// const date = days + 1;
+		// const isoMonth = month + 1;
+		// const bnYear = bnYearBase - YEAR_OFFSET;
+
+		// const DAY = BN_DAYS[this.weekDay];
+		// const MONTH = BN_MONTHS[month];
+
+		// switch (locale) {
+		// 	case 'en':
+		// 		return {
+		// 			year: bnYear,
+		// 			month: isoMonth,
+		// 			date: date,
+		// 			monthName: MONTH.en,
+		// 			dayName: DAY.en,
+		// 			seasonName: getSeason(month, 'en'),
+		// 			isLeapYear: this.isLeapYear(),
+		// 		} as BanglaDate<Locale>;
+
+		// 	default:
+		// 		return {
+		// 			year: digitToBangla(bnYear),
+		// 			month: digitToBangla(isoMonth),
+		// 			date: digitToBangla(date),
+		// 			monthName: MONTH.bn,
+		// 			dayName: DAY.bn,
+		// 			seasonName: getSeason(month),
+		// 			isLeapYear: this.isLeapYear(),
+		// 		} as BanglaDate<Locale>;
+		// }
+
+		return {
+			year: this.getBanglaYear(locale),
+			month: this.getBanglaMonth(locale),
+			date: this.getBanglaDay(locale),
+			monthName: this.getBanglaMonthName(locale),
+			dayName: this.getBanglaDayName(locale),
+			seasonName: this.getBanglaSeasonName(locale),
+			isLeapYear: this.isLeapYear(),
+		} as BanglaDate<Locale>;
 	};
 
 	$Chronos.prototype.formatBangla = function (fmt) {
