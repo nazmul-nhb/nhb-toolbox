c5e5425ad88ce99a22697222010dae280e70eb0d
Nazmul Hassan <nazmulnhb007@yahoo.com>
2026-01-14 20:34:39 +0600

    `refactor(Chronos/plugins):` moved zodiac sorting algorith outside the method to reuse in the future

--- src/date/plugins/zodiacPlugin.ts
+++ src/date/plugins/zodiacPlugin.ts
@@ -24,10 +24,24 @@ declare module '../Chronos' {
 
 /** * Plugin to inject `zodiac` related methods */
 export const zodiacPlugin = ($Chronos: $Chronos): void => {
+	function _toHundreds(range: readonly [number, number]) {
+		return range[0] * 100 + range[1];
+	}
+
+	function _getSortedSigns<Sign extends string = ZodiacSign>(options?: ZodiacOptions<Sign>) {
+		const { preset = 'western', custom } = options ?? {};
+
+		const sorted = [...(custom ?? ZODIAC_PRESETS[preset])].sort(
+			(a, b) => _toHundreds(a[1]) - _toHundreds(b[1])
+		);
+
+		return sorted as ZodiacArray<Sign>;
+	}
+
 	$Chronos.prototype.getZodiacSign = function <Sign extends string = ZodiacSign>(
 		options?: ZodiacOptions<Sign>
 	) {
-		const { birthDate, preset = 'western', custom } = options ?? {};
+		const { birthDate } = options ?? {};
 
 		let month: NumberRange<1, 12>;
 		let date: NumberRange<1, 31>;
@@ -42,27 +56,21 @@ export const zodiacPlugin = ($Chronos: $Chronos): void => {
 			date = this.date;
 		}
 
-		const _toHundreds = (range: readonly [number, number]) => {
-			return range[0] * 100 + range[1];
-		};
+		const sortedSigns = _getSortedSigns(options);
 
-		const signs = [...(custom ?? ZODIAC_PRESETS[preset])].sort(
-			(a, b) => _toHundreds(a[1]) - _toHundreds(b[1])
-		) as ZodiacArray<Sign>;
-
-		for (let i = signs.length - 1; i >= 0; i--) {
-			const [sign, [m, d]] = signs[i];
+		for (let i = sortedSigns.length - 1; i >= 0; i--) {
+			const [sign, [m, d]] = sortedSigns[i];
 
 			if (month > m || (month === m && date >= d)) {
 				return sign;
 			}
 
 			if (i === 0) {
-				return signs.at(-1)![0];
+				return sortedSigns.at(-1)![0];
 			}
 		}
 
-		return signs[0][0];
+		return sortedSigns[0][0];
 	};
 
 	$Chronos.prototype.zodiac = function (options) {
